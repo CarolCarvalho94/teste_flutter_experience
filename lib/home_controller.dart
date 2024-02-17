899908342dd824ba9325bbe81360d5da8da838b8
CarolCarvalho94 <carol.carvalho94@gmail.com>
2024-02-17 15:56:08 -0300

    primeiro commit

---
+++ lib/home_controller.dart
@@ -0,0 +1,11 @@
+
+import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
+
+class HomeController with MessagesStateMixin {
+  
+  void fazerAlgo(){
+    showError('Erro ao fazer algo');
+    
+  }
+
+}
